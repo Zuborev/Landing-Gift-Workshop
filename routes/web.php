73c85009a5a58dcdfdf91474e1506cc53ce46530
Andrey <zuborev.kh@gmail.com>
2020-04-29 00:07:40 +0300

    routes

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,39 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>'web'], function () {
+    Route::match(['get','post'],'/', ['uses'=>'IndexController@execute', 'as' => 'home']);
+    Route::get('/page/{alias}', ['uses'=>'PageController@execute', 'as' => 'pages']);
+
+    Route::auth();
+
+});
+
+//admin
+Route::group(['prefix'=>'admin', 'middleware'=>'auth'], function () {
+  Route::get('/', function () {
+
+  });
+ //admin/pages
+  Route::group(['prefix'=>'pages'], function() {
+        //admin/pages
+      Route::get('/', ['uses'=>'PagesController@execute', 'as'=>'pages']);
+        //admin/pages/add
+      Route::match(['get','post'],'/add', ['uses'=>'PagesAddController@execute', 'as' => 'pagesAdd']);
+        //admin/edit/2
+      Route::match(['get','post', 'delete'],'/edit/{page}', ['uses'=>'PagesEditController@execute', 'as' => 'pagesEdit']);
+  });
+
+   Route::group(['prefix'=>'portfolios'], function() {
+        Route::get('/', ['uses'=>'PortfolioController@execute', 'as'=>'portfolios']);
+        Route::match(['get','post'],'/add', ['uses'=>'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+        Route::match(['get','post', 'delete'],'/edit/{portfolio}', ['uses'=>'PortfolioEditController@execute', 'as' => 'portfolioEdit']);
+    });
+
+    Route::group(['prefix'=>'services'], function() {
+        Route::get('/', ['uses'=>'ServiceController@execute', 'as'=>'services']);
+        Route::match(['get','post'],'/add', ['uses'=>'ServiceAddController@execute', 'as' => 'serviceAdd']);
+        Route::match(['get','post', 'delete'],'/edit/{service}', ['uses'=>'ServiceEditController@execute', 'as' => 'serviceEdit']);
+    });
+
 });
